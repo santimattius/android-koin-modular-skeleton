3d9b37769ff60044978a639e1d22e299e87b723f
Santiago Mattiauda <santimattius@gmail.com>
2024-12-17 20:52:21 -0300

    Koin core coroutines (#1)
    * [feature] lazy modules
    
    * [feature] lazy modules
    
    * [feature] lazy modules example runOnKoinStarted

--- app/src/main/java/com/santimattius/basic/skeleton/MainApplication.kt
+++ app/src/main/java/com/santimattius/basic/skeleton/MainApplication.kt
@@ -1,21 +1,42 @@
 package com.santimattius.basic.skeleton
 
 import android.app.Application
+import android.util.Log
+import com.santimattius.android.first.module.FirstModuleServices
 import com.santimattius.basic.skeleton.di.AppModule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 import org.koin.androix.startup.KoinStartup
 import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.core.runOnKoinStarted
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.ksp.generated.defaultModule
 import org.koin.ksp.generated.module
+import org.koin.mp.KoinPlatform
 
 @OptIn(KoinExperimentalAPI::class)
 class MainApplication : Application(), KoinStartup {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
     override fun onKoinStartup(): KoinAppDeclaration = {
         androidContext(this@MainApplication)
         allowOverride(false)
         modules(AppModule().module)
         defaultModule()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        coroutineScope.launch {
+            val koin = KoinPlatform.getKoin()
+            koin.runOnKoinStarted { koinStarted ->
+                val service = koinStarted.get<FirstModuleServices>()
+                Log.i("MainApplication", "Service: ${service.doSomething()}")
+            }
+        }
     }
 }
--- features/first-module/src/main/java/com/santimattius/android/first/module/FirstModuleInitializer.kt
+++ features/first-module/src/main/java/com/santimattius/android/first/module/FirstModuleInitializer.kt
@@ -1,17 +1,51 @@
 package com.santimattius.android.first.module
 
 import android.content.Context
+import android.util.Log
 import androidx.startup.Initializer
+import kotlinx.coroutines.CoroutineDispatcher
 import org.koin.androix.startup.KoinInitializer
-import org.koin.core.context.loadKoinModules
-import org.koin.ksp.generated.module
+import org.koin.core.Koin
+import org.koin.core.annotation.KoinInternalApi
+//import org.koin.core.context.loadKoinModules
+import org.koin.core.coroutine.KoinCoroutinesEngine
+import org.koin.core.coroutine.KoinCoroutinesEngine.Companion.EXTENSION_NAME
+import org.koin.core.extension.coroutinesEngine
+import org.koin.core.module.Module
+import org.koin.mp.KoinPlatformTools
+import kotlin.time.measureTime
 
 class FirstModuleInitializer : Initializer<Unit> {
+
     override fun create(context: Context) {
-        loadKoinModules(FirstModule().module)
+        //loadKoinModules(lazyFirstModule)
+        val time = measureTime {
+            loadLazyModules(lazyFirstModule)
+        }
+        Log.d(this::class.simpleName, "create: $time")
     }
 
     override fun dependencies(): MutableList<Class<out Initializer<*>>> {
         return mutableListOf(KoinInitializer::class.java)
+    }
+}
+
+@OptIn(KoinInternalApi::class)
+fun loadLazyModules(vararg moduleList: Lazy<Module>, dispatcher: CoroutineDispatcher? = null) {
+    val koin = KoinPlatformTools.defaultContext().get()
+    with(koin) {
+        coroutinesEngine(dispatcher)
+        koin.coroutinesEngine.launchStartJob {
+            koin.loadModules(moduleList.map { it.value })
+        }
+    }
+}
+
+@OptIn(KoinInternalApi::class)
+fun Koin.coroutinesEngine(dispatcher: CoroutineDispatcher? = null) {
+    with(extensionManager) {
+        if (getExtensionOrNull<KoinCoroutinesEngine>(EXTENSION_NAME) == null) {
+            registerExtension(EXTENSION_NAME, KoinCoroutinesEngine(dispatcher))
+        }
     }
 }
